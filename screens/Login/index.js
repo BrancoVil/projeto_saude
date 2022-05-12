8adcf383589c697d75bce33829122785f94e737e
João Vilar <joaovilarrrrr@hotmail.com>
2022-05-11 23:11:09 -0300

    teste
    teste de telas do projeto de marcones

---
+++ screens/Login/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+    View,
+    Text,
+    Button,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+    Dimensions,
+    Image
+} from 'react-native';
+import styles from "./styles";
+import {LinearGradient} from 'expo-linear-gradient';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const Login = () => {
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.text_header}>Bem-Vindo!</Text>
+            </View>
+            <View style={styles.footer}>
+                <Text style={styles.text_footer}>Atendente</Text>
+                <View style={styles.action}>
+                    <FontAwesome
+                        name="user-o"
+                        color="#000"
+                        size={25}
+                        />
+                    <TextInput
+                        placeholder="Usuário"
+                        style={styles.textInput}
+                        autoCapitalize='none'
+                    />
+                </View>
+
+                <Text style={[styles.text_footer, {
+                    marginTop: 35
+                }]}>Senha</Text>
+            <View style={styles.action}>
+                    <FontAwesome
+                        name="lock"
+                        color="#000"
+                        size={28}
+                        />
+                    <TextInput
+                        placeholder="Insira sua senha"
+                        secureTextEntry={true}
+                        style={styles.textInput}
+                        autoCapitalize='none'
+                    />
+                <TouchableOpacity>
+                    <Feather
+                        name="eye"
+                        color="#205DB8"
+                        size={20}
+                    />
+                </TouchableOpacity>
+            </View>
+                <View style={styles.button}>
+                    <LinearGradient
+                        colors={['#205DB8', '#729fe2']}
+                        style={styles.signIn}
+                    >
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('TelaCadastro')}
+                    >
+                        <Text style={styles.textSign}>ENTRAR</Text>
+                    </TouchableOpacity>
+                    </LinearGradient>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default Login
---
+++ screens/Login/styles.js
@@ -0,0 +1,75 @@
+import { StyleSheet } from 'react-native' 
+
+const styles = StyleSheet.create({
+        container: {
+          flex: 1, 
+          backgroundColor: '#205DB8'
+        },
+        header: {
+            flex: 2,
+            justifyContent: 'flex-end',
+            paddingHorizontal: 20,
+            paddingBottom: 90
+        },
+        footer: {
+            flex: 3 ,
+            backgroundColor: '#fff',
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+            paddingHorizontal: 20,
+            paddingVertical: 30
+        },
+        text_header: {
+            color: '#fff',
+            fontWeight: 'bold',
+            fontSize: 40,
+            textAlign:'center',
+            paddingBottom:1
+        },
+        text_footer: {
+            color: '#000',
+            fontSize: 18
+        },
+        action: {
+            flexDirection: 'row',
+            marginTop: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: '#f2f2f2',
+            paddingBottom: 5
+        },
+        actionError: {
+            flexDirection: 'row',
+            marginTop: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: '#FF0000',
+            paddingBottom: 5
+        },
+        textInput: {
+            flex: 1,
+            marginTop: Platform.OS === 'ios' ? 0 : -12,
+            paddingLeft: 10,
+            color: '#000',
+        },
+        errorMsg: {
+            color: '#FF0000',
+            fontSize: 14,
+        },
+        button: {
+            alignItems: 'center',
+            marginTop: 50
+        },
+        signIn: {
+            width: '100%',
+            height: 50,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 10
+        },
+        textSign: {
+            fontSize: 35,
+            fontWeight: 'bold',
+            color:'#fff'
+        }
+      });
+
+  export default styles
